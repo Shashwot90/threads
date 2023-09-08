5e73e0aca8ab7e55e194ecb3d88eae48cf67d241
Shashwot Pradhan <shashwotpradhan@gmail.com>
2023-09-08 08:21:22 +0545

    setting up shad cn for account profile

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -1,8 +1,23 @@
 "use client"
 
-const AccountProfile = () => {
+import { Form } from "@/components/ui/form";
+
+interface Props {
+    user: {
+        id: string;
+        objectId: string;
+        username: string;
+        name: string;
+        bio: string;
+        image: string;
+    };
+    btnTitle: string;
+
+}
+
+const AccountProfile = ({ user, btnTitle }: Props) => {
     return (
-        <div>Account Profile</div>
+        <Form></Form>
     )
 }
 

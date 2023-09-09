24b212f8e5925897099b759ad7724fd0e557c7bd
Shashwot Pradhan <shashwotpradhan@gmail.com>
2023-09-09 06:56:34 +0545

    onsubmit function in accountprofile

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -36,7 +36,7 @@ interface Props {
 
 const AccountProfile = ({ user, btnTitle }: Props) => {
     const [files, setFiles] = useState<File[]>([]);
-
+    const { startUpload} = useUploadThing("media");
     const form = useForm({
         resolver: zodResolver(UserValidation),
         defaultValues: {
@@ -68,11 +68,18 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
 
     }
 
-    function onSubmit(values: z.infer<typeof UserValidation>) {
+    const onSubmit = async (values: z.infer<typeof UserValidation>) => {
         const blob = values.profile_photo;
 
         const hasImageChanged  = isBase64Image(blob);
 
+        if(hasImageChanged) {
+            const imgRes = await  startUpload(files)
+
+            if(imgRes && imgRes[0].fileUrl) {
+                values.profile_photo = imgRes[0].fileUrl;
+            }
+        }
 
 
       }

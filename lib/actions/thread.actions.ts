c2347750c0a151f731ec5a54615533021ef9344c
Shashwot Pradhan <shashwotpradhan@gmail.com>
2023-09-10 07:23:35 +0545

    thread page and action

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -33,3 +33,32 @@ export async function createThread({ text, author, communityId, path}: Params) {
     }
     
 }
+
+export async function fetchPosts(pageNumber = 1, pageSize = 20) {
+    connectToDB();
+
+    //calculate the number of posts to skip 
+    const skipAmount = (pageNumber - 1) * pageSize;
+    
+    //fetch the posts that have no patents( top-level posts)
+    const postsQuery = Thread.find({ parentId: { $in: [null, undefined]}})
+    .sort({ createdAt: 'desc'})
+    .skip(skipAmount)
+    .populate({ path: 'author', model: User })
+    .populate({
+        path: 'children',
+        populate: {
+            path: 'author',
+            model: User,
+            select: "_id name parentId image"
+        }
+    })
+
+    const totalPostsCount = await Thread.countDocuments({ parentId: { $in: [null, undefined]}})
+
+    const posts = await postsQuery.exec();
+
+    const isNext = totalPostsCount > skipAmount + posts.length;
+
+    return { posts, isNext };
+}
